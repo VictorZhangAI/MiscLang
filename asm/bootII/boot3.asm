0ea1dcc234a6dc8aa09a1452683719d80f6f192b
VZstless <2319725508@qq.com>
2025-01-15 10:20:39 +0800

    a more complex bootloader

---
+++ asm/bootII/boot3.asm
@@ -0,0 +1,88 @@
+bits 16
+org 0x7c00
+
+boot:
+mov ax, 0x2401
+int 0x15
+
+mov ax, 0x3
+int 0x10
+
+mov [disk], dl
+
+mov ah, 0x2
+mov al, 1
+mov ch, 0
+mov dh, 0
+mov cl, 2
+mov dl, [disk]
+mov bx, copy_target
+int 0x13
+cli
+lgdt [gdt_pointer]
+mov eax, cr0
+or eax, 0x1
+mov cr0, eax
+mov ax, DATA_SEG
+mov ds, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
+mov ss, ax
+jmp CODE_SEG:boot2
+
+gdt_start:
+dq 0x0
+
+gdt_code:
+dw 0xFFFF
+dw 0x0
+db 0x0
+db 0b10011010
+db 0b11001111
+db 0x0
+
+gdt_data:
+dw 0xFFFF
+dw 0x0
+db 0x0
+db 0b10010010
+db 0b11001111
+db 0x0
+
+gdt_end:
+gdt_pointer:
+dw gdt_end - gdt_start
+dd gdt_start
+
+disk:
+db 0x0
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
+
+copy_target:
+bits 32
+hello: db "Hello in more than 512B world!", 0
+
+boot2:
+mov esi, hello
+mov ebx, 0xB8000
+
+.loop:
+lodsb
+or al, al
+jz halt
+or eax, 0x0F00
+mov word [ebx], ax
+add ebx, 2
+jmp .loop
+
+halt:
+cli
+hlt
+
+times 1024 - ($ - $$) db 0
