920ec33647523dffc9ef97386c1845a2545ec0ba
VZstless <2319725508@qq.com>
2025-02-11 16:01:12 +0800

    added prefix accumulations

---
+++ cpp/leetcode/lc1109.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> res(n, 0);
+        for(auto &v : bookings)
+        {
+            res[v[0] - 1] += v[2];
+            if(v[1] < n) res[v[1]] -= v[2];
+        }
+        for(int i = 1; i < n; i++) res[i] += res[i - 1];
+        return res;
+    }
+};
